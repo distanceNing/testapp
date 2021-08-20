6aa16d0cbcb81b57e31dc20d7008aef79e0bb668
ninggyang <ninggyang@tencent.com>
2021-08-20 17:40:55 +0800

    handler

--- common/statemachine.go
+++ common/statemachine.go
@@ -6,19 +6,25 @@ import (
 
 type State int
 
-type StateNode interface {
-	Do(req interface{}, rsp interface{}, ext map[string]interface{}) (State, ErrorCode)
-	State() State
+type NodeHandler func(req interface{}, rsp interface{}, ext map[string]interface{}) (State, ErrorCode)
+
+type StateNode struct {
+	Do    NodeHandler
+	State State
+}
+
+func NewStateNode(state State, do NodeHandler) *StateNode {
+	return &StateNode{Do: do, State: state}
 }
 
 type StateMachine struct {
-	states map[State]StateNode
+	states map[State]*StateNode
 	begin  State
 	end    State
 }
 
 func NewStateMachine() *StateMachine {
-	return &StateMachine{states: make(map[State]StateNode)}
+	return &StateMachine{states: make(map[State]*StateNode)}
 }
 
 func (s *StateMachine) SetBeginState(state State) {
@@ -32,8 +38,8 @@ func (s *StateMachine) GetBeginState() State {
 	return s.begin
 }
 
-func (s *StateMachine) AddState(node StateNode) {
-	s.states[node.State()] = node
+func (s *StateMachine) AddState(node *StateNode) {
+	s.states[node.State] = node
 }
 
 func (s *StateMachine) Run(cur *State, req interface{}, rsp interface{}) ErrorCode {
--- main.go
+++ main.go
@@ -13,51 +13,29 @@ const (
 	s4 = 3
 )
 
-type Node1 struct {
-}
-
-func (n *Node1) Do(req interface{}, rsp interface{}, ext map[string]interface{}) (common.State, common.ErrorCode) {
+func DoS1(req interface{}, rsp interface{}, ext map[string]interface{}) (common.State, common.ErrorCode) {
 	log.Println("s1 next s2")
 	return s2, common.NewSuccCode()
 }
 
-func (n *Node1) State() common.State {
-	return s1
-}
-
-type Node2 struct {
-}
-
-func (n *Node2) Do(req interface{}, rsp interface{}, ext map[string]interface{}) (common.State, common.ErrorCode) {
+func DoS2(req interface{}, rsp interface{}, ext map[string]interface{}) (common.State, common.ErrorCode) {
 	log.Println("s2 next s3")
 	ext["s3data"] = "xxxx"
 	return s3, common.NewSuccCode()
 }
 
-func (n *Node2) State() common.State {
-	return s2
-}
-
-type Node3 struct {
-}
-
-func (n *Node3) Do(req interface{}, rsp interface{}, ext map[string]interface{}) (common.State, common.ErrorCode) {
+func DoS3(req interface{}, rsp interface{}, ext map[string]interface{}) (common.State, common.ErrorCode) {
 	log.Printf("s3 next s4 , ext[%v]", ext)
 	return s4, common.NewSuccCode()
 }
 
-func (n *Node3) State() common.State {
-	return s3
-}
-
 func flowTest() {
 	sm := common.NewStateMachine()
-	sm.AddState(&Node1{})
-	sm.AddState(&Node2{})
-	sm.AddState(&Node3{})
+	sm.AddState(common.NewStateNode(s1, DoS1))
+	sm.AddState(common.NewStateNode(s2, DoS2))
+	sm.AddState(common.NewStateNode(s3, DoS3))
 	sm.SetBeginState(s1)
 	sm.SetEndState(s4)
-
 	exec := common.NewExecutor()
 	exec.RegisterFlow("test", sm)
 	exec.ProcRequest("test", 1, 2)
